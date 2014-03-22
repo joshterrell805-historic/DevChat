275c787d874d8b85fcd17b678523a003f8bd3e7d
Josh Terrell <joshterrell805@gmail.com>
2014-03-21 18:16:51 -0700

    Specify username
    The user specifies their username before they are allowed to send and receive
     messages.

--- main.njs
+++ main.njs
@@ -8,19 +8,41 @@ function connect(socket)
    new Socket(socket);
 }
 
-var connectionId = 0;
-
 function Socket(socket)
 {
    this.socket = socket;
-   this.username = ++connectionId;
+   this.loggedIn = false;
 
    socket.on("user message", this.receiveMessage.bind(this));
+   socket.on("login", this.login.bind(this));
 }
 
 
-Socket.prototype.receiveMessage = function receiveMessage(data)
+Socket.prototype.receiveMessage = function receiveMessage(message)
 {
-   sio.sockets.emit("user message", {username: this.username, message: data});
+   // only emit messages from sockets that are logged in
+   if (this.loggedIn)
+   {
+      // emit message to all logged in sockets
+      sio.sockets.in("loggedIn").emit(
+         "user message",
+         {username: this.username, message: message}
+      );
+   }
 }
 
+Socket.prototype.login = function login(username)
+{
+   switch (username)
+   {
+      case "josh":
+      case "test":
+         this.loggedIn = true;
+         this.username = username;
+         this.socket.join("loggedIn");
+         this.socket.emit("login", {success: true});
+         break;
+      default:
+         this.socket.emit("login", {message: "Invalid username."});
+   }
+}
--- public/sendText.js
+++ public/sendText.js
@@ -1,23 +1,34 @@
 var socket;
-var input;
+var messageInput;
+var usernameInput;
 
 $(document).ready(
    function()
    {
       socket = io.connect();
-      input = $("#messageInput");
+      messageInput = $("#messageInput");
+      usernameInput = $("#usernameInput");
       socket.on("user message", receiveMessage);
+      socket.on("login", loginResponse);
    }
 );
 
 $(document).keypress(
    function(ev)
    {
-      // When the user preses Enter and is currently focused on the input,
-      // Send the message.
-      if (ev.which == KeyEvent.DOM_VK_RETURN && input.is(":focus"))
+      // When the user preses Enter and is currently focused on #messageInput,
+      // send the message.
+      if (ev.which == KeyEvent.DOM_VK_RETURN && messageInput.is(":focus"))
       {
          sendMessage();
+         return false;
+      }
+
+      // When the user preses Enter and is currently focused on #usernameInput,
+      // login.
+      if (ev.which == KeyEvent.DOM_VK_RETURN && usernameInput.is(":focus"))
+      {
+         login();
          return false;
       }
    }
@@ -26,11 +37,39 @@ $(document).keypress(
 // Send the text that is currently in the input box and clear its text
 function sendMessage()
 {
-   socket.emit("user message", input.val());
-   input.val("");
+   socket.emit("user message", messageInput.val());
+   messageInput.val("");
 }
 
 function receiveMessage(data)
 {
-   console.log(data);
+   console.log(data.username + ": " + data.message);
+}
+
+
+// Attempt to login.
+function login()
+{
+   // only login if the user has provided a username
+   if (usernameInput.val().length)
+   {
+      socket.emit("login", usernameInput.val());
+   }
+}
+
+// The server's response of whether or not the login is good.
+function loginResponse(data)
+{
+   // The user passed authentication
+   if (data.success)
+   {
+      $("#loginPanel").hide();
+      $("#messagePanel").show();
+      messageInput.val("");
+   }
+   // The user failed authentification
+   else
+   {
+      alert(data.message);
+   }
 }
